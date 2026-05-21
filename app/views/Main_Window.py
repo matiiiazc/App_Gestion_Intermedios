89278945e5270ad5f4cff72f7db509f7c9f10c12
Your Name <you@example.com>
2026-05-21 20:00:11 -0300

    Arregllos en la view

--- app/views/Main_Window.py
+++ app/views/Main_Window.py
@@ -22,9 +22,6 @@ class MainWindow(QMainWindow):
         self.menu.addItem(QListWidgetItem("Clientes"))
         self.menu.addItem(QListWidgetItem("Pedidos"))
         self.menu.addItem(QListWidgetItem("Presupuestos"))
-        self.menu.addItem(QListWidgetItem("Facturas"))
-        self.menu.addItem(QListWidgetItem("Productos / Servicios"))
-        self.menu.addItem(QListWidgetItem("Emisor"))
         self.menu.currentRowChanged.connect(self.cambiar_pagina)
 
         sidebar = QFrame()
